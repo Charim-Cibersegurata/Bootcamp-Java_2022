8d5bf309f2de3cd226df028b52851cbc4f8cc028
Charim-Cibersegurata <lasagnatruck1@gmail.com>
2022-09-30 09:43:58 +0200

    Ejercicios UD02 java

---
+++ UD02-JAVA_IDE/ex2.java
@@ -0,0 +1,19 @@
+public class ClaseInteger1{
+	public static void main(String args[]){
+		//Creación de un objeto Integer usando el segundo constructor 
+		Integer numl-new Integer("125");
+		//Creación de un objeto Integer usando el primer constructor 
+		Integer num2-nev Integer (20)
+		//Obtención del entero que almacena cada objeto Integer 
+		//Si no se hace esto, las lineas siguientes causarian error 
+		//de compilación 
+		int nl-num1.int Value(); 
+		int n2-nun 2. intValue(); 
+		System.out.println("Suna de "+n1+" y "+n2+" vale "+(1+n2)); 
+		if (nl+n2>130);
+			nl++;
+		else
+			n --;
+		System.out.println(nl):
+	}
+}
